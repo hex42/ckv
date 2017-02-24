042e1072e214c21d0f851cd25a6aaa4af1e29d69
weychen <weychen@tencent.com>
2017-02-24 20:12:45 +0800

    @o@

--- src/kv/Cache.go
+++ src/kv/Cache.go
@@ -52,7 +52,7 @@ func (l *LRU) Put(e *LRUItem) {
 	l.head = e	
 }
 
-func (l *LRU) Del()  {
+func (l *LRU) Del()  *LRUItem {
 	if l.head == l.tail {
 		l.head = l.tail = nil
 	}
@@ -61,6 +61,7 @@ func (l *LRU) Del()  {
 	l.tail = et.previous
 	et.previous.next = nil
 	et.previous = nil
+	return et
 
 }
 
@@ -93,8 +94,11 @@ func (l *LRU) Update(e *LRUItem) {
 	
 }
 
-
+// kv pair的值不能为空
 func (c *KVStore) Put(key, value string) bool {
+	if value == "" {
+		return false
+	}
 	c.mu.Lock()
 	defer c.mu.UnLock()
 	record := NewRecord("P", key, value)
@@ -106,15 +110,16 @@ func (c *KVStore) Put(key, value string) bool {
 	index[key] = offset
 	mem[key] = e
 	memSize += record.Size()
-	if memSize > memCapacity {
-		c.lru.Del()
+	for  memeSize > memCapacity {
+		e := c.lru.Del()
+		memSize -= e.record.Size()
 	}
 	c.lru.Put(e)
 	return true
 
 }
 
-
+// 某个kv对不存在有两种可能，一直以前就没有存过这个kv对，另一种是以前存过但是后来被删除了
 func (c *KVStore) Get(key string) string {
 	c.mu.Lock()
 	defer c.mu.UnLock()
@@ -122,14 +127,19 @@ func (c *KVStore) Get(key string) string {
 	if ok {
 		return e.record.value
 	}
-	offset := index[key]
-        record := c.l.ReadAt(offset)
-	if record.value == "" {
-	// handle the deleted k v pair
+	offset, ok := index[key]
+	// 从来没有存过
+	if !ok {
+		return ""
 	}
-	mem[key] = value
+    record := c.l.ReadAt(offset)
 	memSize += record.Size()
-        return record.value
+	for  memeSize > memCapacity {
+		e := c.lru.Del()
+		memSize -= e.record.Size()
+	}
+	c.lru.Put(NewLRUItem(record))
+	return record.value
 	
 }
 
@@ -138,29 +148,57 @@ func (c *KVStore) Del(key string) bool {
 	c.mu.Lock()
 	defer c.mu.UnLock()
 	record := NewRecord("D", key, "")
+	e := NewLRUItem(record)
 	offset := c.log.Append(record)
-	index[key] = offset
-	map[key] = ""
-	mem.Size += record.Size()
+	c.index[key] = offset
+	c.mem[key] = e
+	c.memSize += record.Size()
+	for  memeSize > memCapacity {
+		e := c.lru.Del()
+		memSize -= e.record.Size()
+	}
+	c.lru.Put(e)
+	return true
 }
 
 func (c *KVStore) Snapshot() bool {
+
+	//找出最小的offset
 	
 
 }
 
+
 func (c *KVStore) build() bool {
-	
-	
+	fmt.Println("building kv cache")
+	logFiles := c.l.AllLogFiles()
+	for logFile := rang logFiles {
+		fmt.Println("building from %s\n", logFile)
+		records, offsets := c.l.ReadLogFile(logFile)
+		for i:=0; i< len(records); i+=1 {
+			r := records[i]
+			map[r.key] = offsets[i]
+
+		}
+	}
+	fmt.Printf("building kv cacahe successful")
 }
 
 func (c *KVStore) shrink() bool {
+	fmt.Printlnn("shrink")
+	
 	
 
 }
 
 
-func NewCache() *Cache {
+func NewCache(dir string, isLogSync bool, logSyncSize int64, kvMemCapacity int64) *Cache {
+	log := NewLog(dir, isLogSync, logSyncSize)
+	kvStore := x,x,ddkd
+	
+	kvStore.build()
+
+	return kvStore
 	
 }
 
